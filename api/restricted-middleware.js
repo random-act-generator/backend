e1581e8dbb9ad6a654078d75d873ca64f468ea01
Tevvin <Tevvin7395@icloud.com>
2019-07-10 23:06:38 -0400

    restricted-middleware finished

---
+++ api/restricted-middleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const secrets = require('../config/secrets.js');
+
+module.exports = (req, res, next) => {
+    const token = req.req.headers.Authorization;
+
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+        if (err) {
+            res.status(401).json({ message: "You shall not pass son!" })
+            //console.log(token);
+        } else {
+            req.decodedToken = decodedToken;
+
+            next();
+        }
+    });
+};
