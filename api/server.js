6b7e7697e0ada835885a06ef19c598424eeb5361
Tevvin <Tevvin7395@icloud.com>
2019-07-11 23:23:09 -0400

    Server.js finished

---
+++ api/server.js
@@ -0,0 +1,23 @@
+// Imports
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+// Connect to routers
+const authRouter = require('./auth-router');
+const userRouter = require('./user-router');
+
+Server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use('/api', userRouter)
+// Not sure if this is need below.
+server.use('/api', authRouter)
+
+// Test route
+server.get('/', (req, res) => {
+    res.send("<h1>Test works</h1>");
+});
+
+module.exports = server;
