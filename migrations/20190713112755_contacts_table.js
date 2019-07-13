7f5bea090bfa176a8e07dbbd8a917ce77179c0c0
Christian-Ford <fjord.christian@gmail.com>
2019-07-13 11:33:52 -0500

    finished migrations

--- migrations/20190713112755_contacts_table.js
+++ migrations/20190713112755_contacts_table.js
@@ -1,8 +1,25 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', users => {
+    users
+      .increments();
+
+    users 
+      .string('username', 128)
+      .notNullable()
+      .unique();
+
+    users
+      .string('contactName', 128)
+      .notNullable();
+
+    users
+      .string('contactEmail', 128)
+      .notNullable()
+      
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('users');
 };
