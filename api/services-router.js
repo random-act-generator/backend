a476da3d12c81d9294100381a3aa372276bdf220
Christian-Ford <fjord.christian@gmail.com>
2019-07-19 19:06:16 -0500

    Updated services model and router

--- api/services-router.js
+++ api/services-router.js
@@ -1,16 +1,52 @@
 const router = require('express').Router();
 const Services = require('./services-model.js');
 
-const restricted = require('./restricted-middleware.js');
-
-router.get('/services', restricted, (req, res) => {
+// get contacts
+router.get('/services', (req, res) => {
     Services.get()
         .then(services => {
-            res.json({ services, decodedToken: req.decodedToken });
+            res.status(201).json(services)
         })
-        .catch(error => {
-            //res.status(500).json({ error: "We ran into an error retreving the specified request.", error });
-            res.send(error);
+        .catch(err => {
+            res.status(404).json({ message: "Please try again" });
+        });
+});
+
+// post contacts
+router.post('/services', (req, res) => {
+    let newService = req.body;
+
+    Contacts.add(newService)
+        .then(service => {
+            res.status(201).json(service);
+        })
+        .catch(err => {
+            res.status(404).json(err)
+        });
+});
+
+// update contacts
+router.put('/services/:id', (req, res) => {
+    Services.update(req.params.id, req.body)
+
+        .then(updated => {
+            res.status(201).json(updated);
+        })
+        .catch(err => {
+            res.status(404).json(err);
+        });
+});
+
+// delete contacts
+router.delete('/services/:id', (req, res) => {
+    let deleteService = req.params.id;
+
+    Contacts.remove(deleteService)
+        .then(deleted => {
+            res.status(201).json(deleted);
+        })
+        .catch(err => {
+            res.status(404).json(err);
         });
 });
 
